06765d7a8dae9f1cb09f36e0441e4fc25a3afd89
Michael Nero <michaelnero@outlook.com>
2023-07-20 21:23:15 +0000

    adding additional vercel domains.

--- svelte.config.js
+++ svelte.config.js
@@ -10,7 +10,7 @@ const config = {
     csp: {
       mode: "auto",
       directives: {
-        "connect-src": ["self", "vitals.vercel-insights.com"],
+        "connect-src": ["self", "vitals.vercel-insights.com", "vercel.live"],
       }
     }
   }
